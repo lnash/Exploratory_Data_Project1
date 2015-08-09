d09e2c0270bb42395509bc139be8848f23681065
lnash <l_nash@hotmail.co.uk>
2015-08-09 18:57:11 +0100

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,25 @@
+##read data
+
+data <- read.table("C:/Users/nashlaur.CSR/Documents/household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+##subset& format
+
+datetime <- strptime(paste(subset$Date, subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+GlobalActivePower <- as.numeric(subset$Global_active_power)
+
+subMetering1 <- as.numeric(subset$Sub_metering_1)
+subMetering2 <- as.numeric(subset$Sub_metering_2)
+subMetering3 <- as.numeric(subset$Sub_metering_3)
+
+##plot data
+
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+
+##save file
+
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
